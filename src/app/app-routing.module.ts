b8c516373bf9e903cbb1d832e11763431ee8a575
Josh Robinson <josh.robinson@gmail.com>
2018-01-26 10:14:02 -0500

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent},
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail:id', component: HeroDetailComponent},
+  { path: 'detail/:id', component: HeroDetailComponent},
   { path: '', redirectTo: '/dashboard', pathMatch:'full'}
 
 ];
